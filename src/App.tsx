8945062036f9edf854c037db538cd80f819ae727
i <i@alexandermaru.com>
2021-10-22 01:43:27 +0000

    Layout app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react'
-import './App.css'
+import { ProvideAuth } from './auth/use-auth'
+import './assets/stylesheets/App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  )
+  return <ProvideAuth />
 }
 
 export default App
