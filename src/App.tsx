3afc28a9023b0b0c30aa1467abf6bd307c18f111
i <i@alexandermaru.com>
2021-10-29 05:40:21 +0000

    Clean slate - before

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ProvideAuth } from './auth/use-auth'
-import './assets/stylesheets/App.css'
-
-function App() {
-  return <ProvideAuth />
-}
-
-export default App
--- src/pages/JobsCreate.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function JobsCreate() {
-  return <h2>JobsCreate</h2>
-}
-
-export default JobsCreate
