872ef6a11b55bf87571d4aab3f0d0c30c0af113c
i <i@alexandermaru.com>
2021-11-11 01:37:48 +0000

    Create base URL based on current location origin

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,6 +1,21 @@
 import { Auth } from './types'
 
-export const baseUrl = 'http://0.0.0.0:3001'
+const createBaseUrl = (): string => {
+  const locationToMatchRegex = new RegExp(
+    '^https://[a-zA-Z0-9-]*.sandboxes.run'
+  )
+
+  if (
+    window.location.protocol === 'https:' ||
+    locationToMatchRegex.test(window.location.origin)
+  ) {
+    return window.location.origin
+  }
+
+  return 'http://localhost:3001'
+}
+
+export const baseUrl = createBaseUrl()
 
 export const unauthenticated: Auth = {
   redirectPath: '',
